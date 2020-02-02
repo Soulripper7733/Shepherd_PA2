925f10b50378d82ca0057684dea50a02ed328672
Tyler Shepherd <tyman7733@gmail.com>
2020-02-01 23:01:11 -0500

    updated BMI

--- shepherd_p2/src/BMICalculator.java
+++ shepherd_p2/src/BMICalculator.java
@@ -1,9 +1,68 @@
+import java.util.Scanner;
 
+/* Get the user input for their height and weight
+ *
+ * */
+ 
 public class BMICalculator {
+	static Scanner us = new Scanner(System.in);
+public static void readUserData() {
+	
+	getWeight();
+	getHeight();
+	
+}
+private static readUnitType() {
+	
+	return ;
+}
+public static readMeasurmentData() {
+	return ;
+}
+private readMetricData() {
+	
+}
+private readImperialData() {
+	
+}
+public static calculateBMI() {
+	
+}
+private BmiCategory() {
+	
+}
+
+public static void getWeight() {
+	int Weight;
+	System.out.println("Please enter your Weight : ");
+	Weight = us.nextInt();
+}
+public int setWeight(int Weight) {
+	
+	return Weight;
+}
+public static void getHeight() {
+	int Height;
+	System.out.println("Please enter your Height : ");
+	Height = us.nextInt();
+}
+public int setHeight(int Height) {
+	
+	return Height; 
+} 
+public get Bmi() {
+	
+}
+public getBmiCategorey(){
+	
+}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		  BMICalculator app = new BMICalculator();
+		    app.readUserData();
+		    app.calculateBmi();
+		    app.displayBmi();
 	}
 
 }
